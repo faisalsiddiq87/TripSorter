6f5a5855cb3cf74733b81973a5422507147b122a
faisalsiddiq87 <faisal.siddiq87@gmail.com>
2016-10-14 01:50:51 +0500

    Property Finder test first commit

---
+++ propertyFinder/tests/ParserTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace tests;
+
+use app\Core\Parser;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * Class ParserTest
+ *
+ * The class tests the json file parsing functionality
+ *
+*/
+class ParserTest extends \PHPUnit_Framework_TestCase
+{
+    protected $parser;
+	
+    protected $jsonFile = ROOT;
+	
+	/**
+     * setup the test and load main parser class
+     *
+    */
+    public function setUp()
+    {
+        $this->jsonFile .= 'data/sampleData.json';
+        $this->parser = new Parser($this->jsonFile);
+    }
+	
+	/**
+     * convert json string to array and check if its array
+     *
+    */
+    public function testParseJson()
+    {
+        $data = $this->parser->parseJson();
+        $this->assertTrue(is_array($data));
+    }
+}
